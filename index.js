a9d260565017e095f20b025dab061c6e5a71fbdb
khristine-g <githigekhristine@gmail.com>
2023-03-15 20:00:55 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+  }
+
+function overwriteBestCustomer(newBestCustomer) {
+    window.bestCustomer = newBestCustomer;
+  }
+
+const leastFavoriteCustomer = 'Leah';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Ann'; 
+  }
+  
